d066192e4665c2bf9538c07be9a3ead3ad853832
Sarah Ridge <sarah@cobalt.io>
2021-06-21 14:33:13 -0400

    Add v1 and v2 article endpoints

---
+++ spec/sandbox_5_2/app/controllers/v2/articles_controller.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+module V2
+  class ArticlesController < ApplicationController
+    # GET /articles
+    def index
+      render json: {
+        data: [
+          {
+            type: "articles",
+            id: "1",
+            attributes: {
+              title: "article1", body: "anything1"
+            }
+          },
+          {
+            type: "articles",
+            id: "2",
+            attributes: {
+              title: "article2", body: "anything2"
+            }
+          }
+        ]
+      }
+    end
+
+    # POST /articles
+    def create
+      render json: {
+        data: {
+          type: "articles",
+          id: "1",
+          attributes: {
+            title: params[:title].to_s, body: params[:body].to_s
+          }
+        }
+      }, status: :created
+    end
+
+    # GET /articles/:id
+    def show
+      render json: {
+        data: {
+          type: "articles",
+          id: "1",
+          attributes: {
+            title: "article1", body: "anything"
+          }
+        }
+      }
+    end
+
+    # PATCH /articles/:id
+    def update
+      render json: {
+        data: {
+          type: "articles",
+          id: "1",
+          attributes: {
+            title: params[:title].to_s, body: params[:body].to_s
+          }
+        }
+      }
+    end
+
+    # DELETE /articles/:id
+    def destroy
+      head :no_content
+    end
+  end
+end
--- spec/sandbox_5_2/config/routes.rb
+++ spec/sandbox_5_2/config/routes.rb
@@ -2,4 +2,12 @@
 
 Rails.application.routes.draw do
   resources :articles
+
+  namespace :v1 do
+    resources :articles
+  end
+
+  namespace :v2 do
+    resources :articles
+  end
 end
