f0fc936f4196bcaf45d081a0e35dda78ff0ef556
Sarah Ridge <sarah@cobalt.io>
2021-07-16 15:29:11 -0400

    Add task spec in sandbox app

--- spec/sandbox_5_2/spec/tasks/swagcov_spec.rb
+++ spec/sandbox_5_2/spec/tasks/swagcov_spec.rb
@@ -4,4 +4,10 @@ describe "rake swagcov", type: :task do
   it "preloads the Rails environment" do
     expect(task.prerequisites).to include "environment"
   end
+
+  it "exits normally" do
+    task
+  rescue SystemExit => e
+    expect(e.status).not_to eq(0)
+  end
 end
