5d69d6d8085c1d2b66f08483cfa3ea476fa9a3b2
Sarah Ridge <sarahmarie@hey.com>
2025-03-07 14:19:35 -0700

    Add swagcov specs

--- spec/sandbox_7_2/config/routes.rb
+++ spec/sandbox_7_2/config/routes.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :articles
+  resources :users
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  namespace :v1 do
+    resources :articles
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  namespace :v2 do
+    resources :articles
+  end
 end
