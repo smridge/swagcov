0831c3c2c716cd59ce668ac22c27421addf16f8d
Sarah Ridge <ridge.sarah@gmail.com>
2025-03-14 21:25:24 -0700

    Remove database setup for rails 8 sandbox (#51)

--- spec/sandbox_8_0/spec/spec_helper.rb
+++ spec/sandbox_8_0/spec/spec_helper.rb
@@ -2,23 +2,17 @@
 
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
-# Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+# rspec config.use_active_record not working as expected
+# https://github.com/rspec/rspec-rails/issues/1876
+Object.send(:remove_const, :ActiveRecord) # rubocop:disable RSpec/RemoveConst
+
 require "rspec/rails"
 
 Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
 
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
-end
-
 RSpec.configure do |config|
-  config.fixture_paths = ["#{Rails.root}/spec/fixtures"]
-  config.use_transactional_fixtures = true
-  config.infer_spec_type_from_file_location!
+  config.use_transactional_fixtures = false
+  config.use_active_record = false
   config.filter_rails_from_backtrace!
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
