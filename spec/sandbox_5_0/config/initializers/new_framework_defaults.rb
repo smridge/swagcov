cd35678554cd847ee42faf47a8dd4d6d9d2ede30
Sarah Ridge <ridge.sarah@gmail.com>
2025-03-17 16:54:52 -0700

    Remove database setup for Rails 5 sandbox applications (#53)

--- spec/sandbox_5_0/config/initializers/new_framework_defaults.rb
+++ spec/sandbox_5_0/config/initializers/new_framework_defaults.rb
@@ -12,9 +12,6 @@ Rails.application.config.action_controller.raise_on_unfiltered_parameters = true
 # Previous versions had false.
 ActiveSupport.to_time_preserves_timezone = true
 
-# Require `belongs_to` associations by default. Previous versions had false.
-Rails.application.config.active_record.belongs_to_required_by_default = true
-
 # Do not halt callback chains when a callback returns false. Previous versions had true.
 ActiveSupport.halt_callback_chains_on_return_false = false
 
