1f78d73000c0401d9b7d9e1236a1dabc524b4e99
Sarah Ridge <sarahmarie@hey.com>
2025-04-09 17:43:34 -0700

    Update test stubs + minor test cleanup

--- spec/sandbox_6_0/spec/tasks/swagcov_spec.rb
+++ spec/sandbox_6_0/spec/tasks/swagcov_spec.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 describe "rake swagcov", type: :task do
-  it "preloads the Rails environment" do
-    expect(task.prerequisites).to include "environment"
-  end
+  it { expect(task.prerequisites).to include "environment" }
 
   context "with minimal configuration and full documentation coverage" do
     it "outputs coverage" do
@@ -11,38 +9,40 @@ describe "rake swagcov", type: :task do
         task.execute
       rescue SystemExit => e # ignore to test output
         e.inspect
-      end.to output(<<~MESSAGE).to_stdout
-               GET /articles         #{200.to_s.green}
-              POST /articles         #{201.to_s.green}
-               GET /articles/:id     #{200.to_s.green}
-             PATCH /articles/:id     #{200.to_s.green}
-               PUT /articles/:id     #{200.to_s.green}
-            DELETE /articles/:id     #{204.to_s.green}
-               GET /users            #{200.to_s.green}
-              POST /users            #{201.to_s.green}
-               GET /users/:id        #{200.to_s.green}
-             PATCH /users/:id        #{200.to_s.green}
-               PUT /users/:id        #{200.to_s.green}
-            DELETE /users/:id        #{204.to_s.green}
-               GET /v1/articles      #{200.to_s.green}
-              POST /v1/articles      #{201.to_s.green}
-               GET /v1/articles/:id  #{200.to_s.green}
-             PATCH /v1/articles/:id  #{200.to_s.green}
-               PUT /v1/articles/:id  #{200.to_s.green}
-            DELETE /v1/articles/:id  #{204.to_s.green}
-               GET /v2/articles      #{200.to_s.green}
-              POST /v2/articles      #{201.to_s.green}
-               GET /v2/articles/:id  #{200.to_s.green}
-             PATCH /v2/articles/:id  #{200.to_s.green}
-               PUT /v2/articles/:id  #{200.to_s.green}
-            DELETE /v2/articles/:id  #{204.to_s.green}
+      end.to output(
+        <<~MESSAGE
+                 GET /articles         #{200.to_s.green}
+                POST /articles         #{201.to_s.green}
+                 GET /articles/:id     #{200.to_s.green}
+               PATCH /articles/:id     #{200.to_s.green}
+                 PUT /articles/:id     #{200.to_s.green}
+              DELETE /articles/:id     #{204.to_s.green}
+                 GET /users            #{200.to_s.green}
+                POST /users            #{201.to_s.green}
+                 GET /users/:id        #{200.to_s.green}
+               PATCH /users/:id        #{200.to_s.green}
+                 PUT /users/:id        #{200.to_s.green}
+              DELETE /users/:id        #{204.to_s.green}
+                 GET /v1/articles      #{200.to_s.green}
+                POST /v1/articles      #{201.to_s.green}
+                 GET /v1/articles/:id  #{200.to_s.green}
+               PATCH /v1/articles/:id  #{200.to_s.green}
+                 PUT /v1/articles/:id  #{200.to_s.green}
+              DELETE /v1/articles/:id  #{204.to_s.green}
+                 GET /v2/articles      #{200.to_s.green}
+                POST /v2/articles      #{201.to_s.green}
+                 GET /v2/articles/:id  #{200.to_s.green}
+               PATCH /v2/articles/:id  #{200.to_s.green}
+                 PUT /v2/articles/:id  #{200.to_s.green}
+              DELETE /v2/articles/:id  #{204.to_s.green}
 
-        OpenAPI documentation coverage 100.00% (24/24)
-        #{0.to_s.yellow} ignored endpoints
-        #{24.to_s.blue} total endpoints
-        #{24.to_s.green} covered endpoints
-        #{0.to_s.red} uncovered endpoints
-      MESSAGE
+          OpenAPI documentation coverage 100.00% (24/24)
+          #{0.to_s.yellow} ignored endpoints
+          #{24.to_s.blue} total endpoints
+          #{24.to_s.green} covered endpoints
+          #{0.to_s.red} uncovered endpoints
+        MESSAGE
+      ).to_stdout
     end
 
     context "when testing final result" do
@@ -56,10 +56,7 @@ describe "rake swagcov", type: :task do
 
   context "with full configuration and partial documentation coverage" do
     before do
-      stub_const(
-        "Swagcov::Dotfile::DEFAULT_CONFIG_FILE_NAME",
-        Pathname.new("#{Rails.root}/spec/fixtures/dotfiles/only_and_ignore_config.yml")
-      )
+      stub_const("Swagcov::Dotfile::DEFAULT_CONFIG_FILE_NAME", "spec/fixtures/dotfiles/only_and_ignore_config.yml")
     end
 
     it "outputs coverage" do
@@ -67,24 +64,26 @@ describe "rake swagcov", type: :task do
         task.execute
       rescue SystemExit => e # ignore to test output
         e.inspect
-      end.to output(<<~MESSAGE).to_stdout
-              POST /v1/articles      #{201.to_s.green}
-               GET /v1/articles      #{'ignored'.yellow}
-               GET /v2/articles/:id  #{'ignored'.yellow}
-             PATCH /v2/articles/:id  #{'ignored'.yellow}
-               PUT /v2/articles/:id  #{'ignored'.yellow}
-            DELETE /v2/articles/:id  #{'ignored'.yellow}
-               GET /v1/articles/:id  #{'none'.red}
-             PATCH /v1/articles/:id  #{'none'.red}
-               PUT /v1/articles/:id  #{'none'.red}
-            DELETE /v1/articles/:id  #{'none'.red}
+      end.to output(
+        <<~MESSAGE
+                POST /v1/articles      #{201.to_s.green}
+                 GET /v1/articles      #{'ignored'.yellow}
+                 GET /v2/articles/:id  #{'ignored'.yellow}
+               PATCH /v2/articles/:id  #{'ignored'.yellow}
+                 PUT /v2/articles/:id  #{'ignored'.yellow}
+              DELETE /v2/articles/:id  #{'ignored'.yellow}
+                 GET /v1/articles/:id  #{'none'.red}
+               PATCH /v1/articles/:id  #{'none'.red}
+                 PUT /v1/articles/:id  #{'none'.red}
+              DELETE /v1/articles/:id  #{'none'.red}
 
-        OpenAPI documentation coverage 20.00% (1/5)
-        #{5.to_s.yellow} ignored endpoints
-        #{5.to_s.blue} total endpoints
-        #{1.to_s.green} covered endpoint
-        #{4.to_s.red} uncovered endpoints
-      MESSAGE
+          OpenAPI documentation coverage 20.00% (1/5)
+          #{5.to_s.yellow} ignored endpoints
+          #{5.to_s.blue} total endpoints
+          #{1.to_s.green} covered endpoint
+          #{4.to_s.red} uncovered endpoints
+        MESSAGE
+      ).to_stdout
     end
 
     context "when testing final result" do
