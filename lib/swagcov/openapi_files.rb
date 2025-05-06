e7610850afa3065e427c40913ddf0b3ddeb75994
Sarah Ridge <sarahmarie@hey.com>
2025-05-06 16:23:39 -0700

    Improve performance of response key matching

--- lib/swagcov/openapi_files.rb
+++ lib/swagcov/openapi_files.rb
@@ -5,13 +5,14 @@ module Swagcov
     def initialize filepaths:
       @filepaths = filepaths
       @openapi_paths = load_yamls
+      @openapi_path_keys = @openapi_paths.keys
     end
 
     def find_response_keys path:, route_verb:
       # replace :id with {id}
       regex = ::Regexp.new("^#{path.gsub(%r{:[^/]+}, '\\{[^/]+\\}')}?$")
 
-      matching_paths_key = @openapi_paths.keys.grep(regex).first
+      matching_paths_key = @openapi_path_keys.grep(regex).first
       matching_request_method_key = @openapi_paths.dig(matching_paths_key, route_verb.downcase)
 
       matching_request_method_key["responses"].keys.map(&:to_s).sort if matching_request_method_key
