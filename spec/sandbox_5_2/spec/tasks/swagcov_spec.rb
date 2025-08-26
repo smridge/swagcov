e39d5db096ffec5b01e0ddc6b5ea51a4d4bb75a8
Sarah Ridge <sarahmarie@hey.com>
2025-08-25 17:01:24 -0700

    Improve rake task test speed

--- spec/sandbox_5_2/spec/tasks/swagcov_spec.rb
+++ spec/sandbox_5_2/spec/tasks/swagcov_spec.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-Dir["../sandbox_support/shared_examples/tasks/**/*.rb"].sort.each { |file| require file }
+require "../sandbox_support/shared_examples/tasks/swagcov_spec"
