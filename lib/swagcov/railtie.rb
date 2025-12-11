beda53e050d5278afe29754b3b2fa7d592dfae3b
Sarah Ridge <ridge.sarah@gmail.com>
2025-12-11 12:13:27 -0700

    Resolve constants for loading rake task (#181)

--- lib/swagcov/railtie.rb
+++ lib/swagcov/railtie.rb
@@ -3,9 +3,9 @@
 module Swagcov
   class Railtie < ::Rails::Railtie
     rake_tasks do
-      path = File.expand_path("..", __dir__)
+      path = ::File.expand_path("..", __dir__)
 
-      Dir.glob("#{path}/tasks/**/*.rake").each { |f| load f }
+      ::Dir.glob("#{path}/tasks/**/*.rake").each { |f| load f }
     end
   end
 end
